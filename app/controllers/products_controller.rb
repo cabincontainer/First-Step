d2ecd0e29691eacdbadbd8efdd093196bc5f58bd
Cabin Container <cabincontainer@gmail.com>
2013-04-23 23:38:58 +0700

    show products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,11 @@ class ProductsController < ApplicationController
 	skip_filter :require_user
 
 	def index
+		@products = Product.all
 	end
 
 	def show
+		@product = Product.find(params[:id])
 	end
 
 	def show_porducts
