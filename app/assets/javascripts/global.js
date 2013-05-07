0615ca1437a5c2ee9f79b1c81f0e9fe9bae5f6fd
Cabin Container <cabincontainer@gmail.com>
2013-05-07 22:18:44 +0700

    add fancybox js

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,3 +1,5 @@
+//= require jquery.fancybox.js
+
 Globals = {
   init: function(){
 	$('.bxslider').bxSlider({
@@ -9,6 +11,20 @@ Globals = {
 	//Show Clock
 	$('#analog-clock').clock({offset: '+7', type: 'analog'});
 	//End Clock
+
+	if($('a.fancy_link').length > 0){
+      $('a.fancy_link').fancybox({
+        showCloseButton     : true,
+        hideOnOverlayClick  : true,
+        padding             : 0,
+        openEffect          : 'elastic',
+        openSpeed           : 300,
+        closeEffect         : 'elastic',
+        closeSpeed          : 300,
+        closeClick          : false,
+        overlayShow			: true
+      });
+    }
 
   	var category_id = $("#middle_layout").find("h5").attr("category-id");
   	var cat_id = $("a[cat_id='"+category_id+"']").parents(".products-link").addClass("left-link-active");
