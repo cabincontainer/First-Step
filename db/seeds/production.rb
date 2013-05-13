df754841cbfd94d079ed969f55d4f1744990bcd7
Cabin Container <cabincontainer@gmail.com>
2013-05-14 01:39:46 +0700

    User seed data

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,4 +1,4 @@
 #encoding: utf-8
 
 # Admins
-admin = FactoryGirl.create(:user, name: "KK Container Admin", password: "RadioActive22", email: "kkcontainer@hotmail.com")
+admin = User.create(:user, name: "KK Container Admin", password: "RadioActive22", email: "kkcontainer@hotmail.com")
