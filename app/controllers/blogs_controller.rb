1da43d47236890ff7844da04d5e4b984d76919a6
Artiwarah Damrongchai <artiwarahd@gmail.com>
2016-04-19 16:28:26 +0700

    Manage blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,4 +18,25 @@ class BlogsController < ApplicationController
   def show
     @blog = Blog.find(params[:id])
   end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    @blog.assign_attributes(params[:blog])
+
+    if @blog.save
+      redirect_to blog_path(@blog)
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+
+    if @blog.destroy
+      redirect_to blogs_path
+    end
+  end
 end
