2f56b40223e0e8fe148157a1a02534a2c3ac684f
Artiwarah Damrongchai <artiwarahd@gmail.com>
2016-05-04 02:05:14 +0700

    order products by created_at DESC

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
 	skip_filter :require_user
 
 	def index
-    @random_products = Product.order("created_at ASC").limit(8)
+    @random_products = Product.order("created_at DESC").limit(8)
 	end
 
   def coming_soon; end
